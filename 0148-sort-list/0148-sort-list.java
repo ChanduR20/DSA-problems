42cbb27aeeda2e0cdf6a5e71f668a636f161a983
ChanduR20 <chanduvenkat90@gmail.com>
2025-07-10 10:51:27 +0530

    Time: 9 ms (96.23%), Space: 56.4 MB (71.69%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,21 +9,46 @@
  * }
  */
 class Solution {
+    public ListNode middlelist(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode mergelist(ListNode left,ListNode right){
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+        while(left!=null && right!=null){
+            if(left.val<=right.val){
+                temp.next=left;
+                left=left.next;
+            }
+            else{
+                temp.next=right;
+                right=right.next;
+            }
+            temp=temp.next;
+        }
+        if(left!=null){
+            temp.next=left;
+        }
+        else{
+            temp.next=right;
+        }
+        return dummy.next;
+    }
     public ListNode sortList(ListNode head) {
-        ListNode temp=head;
-        ArrayList<Integer> ans= new ArrayList<>();
-        while(temp!=null){
-            ans.add(temp.val);
-            temp=temp.next;
-        }
-        Collections.sort(ans);
-        temp=head;
-        int i=0;
-        while(temp!=null){
-            temp.val=ans.get(i);
-            i++;
-            temp=temp.next;
-        }
-        return head;
+        if(head==null || head.next==null) return head;
+        ListNode middle=middlelist(head);
+        ListNode right=middle.next;
+        middle.next=null;;
+        ListNode left=head;
+
+        left=sortList(left);
+        right=sortList(right);
+        return mergelist(left,right);
     }
 }
